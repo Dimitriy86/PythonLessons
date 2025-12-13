d20ecd6116a9f871cfec5d70cc78f1375787b688
dmitr <dmitriyshirin86@gmail.com>
2025-12-13 21:04:18 +0100

    Parsing date

---
+++ Module_3/parsing_date.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+#------------------------------------------------------------
+# now = datetime.now()
+
+# Формування дня і часу
+# formatted_day = now.strftime("%Y-%m-%d %H:%M:%S")
+# print(formatted_day)
+
+# Формування лише дати
+# formatted_day_only = now.strftime("%A, %d %B %Y")
+# print(formatted_day_only)
+
+# Формування лише часу
+# formatted_time_only = now.strftime("%I:%M%p")
+# print(formatted_time_only)
+
+# # Формування лише дати
+# formatted_date_only = now.strftime("%d.%m.%Y")
+# print(formatted_date_only)
+
+#------------------------------------------------------------
+
+# Припустимо, у нас є дата у вигляді рядка
+# date_string = "2025.12.13"
+
+# # Перетворення рядка в об'єкт datetime
+# datetime_object = datetime.strptime(date_string, "%Y.%m.%d")
+# print(datetime_object)
+
+#------------------------------------------------------------
+
