e93e1fc7d3467665f19ac119ca20187d04634dc0
dmitr <dmitriyshirin86@gmail.com>
2025-12-13 20:04:43 +0100

    Work with datetime

---
+++ Module_3/work_with_date.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+
+# Створення об'єктів date і time
+# date_part = datetime.now().date()
+# time_part = datetime.now().time()
+# combined_datetime = datetime.combine(date_part, time_part)
+# print(combined_datetime)
+
+#----------------------------------------------------------------
+
+# Створення об'єкта з конкретною датаю
+# specific_date = datetime(year=2025, month=12, day=13)
+# print(specific_date)
+
+# Створення об'єкта з конкретною датаю і часом
+# specific_date = datetime(year=2025, month=12, day=13, hour=19, minute=26, second=25)
+# print(specific_date)
+
+#----------------------------------------------------------------
+
+# Створення об'єкта datetime
+# now = datetime.now()
+
+# Отримання номера для тижня
+# day_of_week = now.weekday()
+
+# Поверне число від 0 (понеділок) до 6 (неділя)
+# print(f"Сьогодні {day_of_week}")
+
+#----------------------------------------------------------------
+
+# Створення двох об'єктів datetime
+# datetime1 = datetime(2025, 12, 12, 19, 33 ,30)
+# datetime2 = datetime(2025, 12, 13, 19, 33, 30)
+
+# Порівняння дат
+# print(datetime1 == datetime2)
+# print(datetime1 != datetime2)
+# print(datetime1 < datetime2)
+# print(datetime1 > datetime2)
+
+#----------------------------------------------------------------
+
