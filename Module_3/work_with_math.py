a0eb4d607d9ea4c32998e2743c7c8873b98e745a
dmitr <dmitriyshirin86@gmail.com>
2025-12-14 13:23:48 +0100

    Work with math

---
+++ Module_3/work_with_math.py
@@ -0,0 +1,30 @@
+import math
+
+# ---------------------------------------------------------------
+
+# Вхідне число
+# x = 3.7
+
+# Використання різних методів округлення
+# ceil_result = math.ceil(x)
+# floor_result = math.floor(x)
+# trunc_result = math.trunc(x)
+
+# print(ceil_result, floor_result, trunc_result)
+
+# ---------------------------------------------------------------
+
+# Використання констант
+# print(math.pi)
+
+# # Тригонометрія
+# angle = math.radians(60)
+# print(math.cos(angle))
+
+# # Корінь числа
+# print(math.sqrt(9))
+
+# # Логарифми
+# print(math.log(10, 2))
+
+# ---------------------------------------------------------------
