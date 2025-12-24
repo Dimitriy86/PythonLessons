a381db45ab905453f04bc6eade44aea314a04cd3
dmitr <dmitriyshirin86@gmail.com>
2025-12-24 11:58:33 +0100

    Work with split

---
+++ Module_3/split_file.py
@@ -0,0 +1,24 @@
+import re
+
+# --------------------------------------------------------
+text = "Python - це проста, але потужна мова програмування."
+pattern = r"\s+"
+words = re.split(pattern, text)
+
+print(words)
+
+# --------------------------------------------------------
+
+text = "Python - потужна; проста, універсальна: мова!"
+pattern = r"[;,\-:!\s]"
+elements = re.split(pattern, text)
+
+print(elements)
+
+# --------------------------------------------------------
+
+text = "apple#banana!mango@orange;kiwi"
+pattern = r"[#@;!]"
+fruits = re.split(pattern, text)
+
+print(fruits)
