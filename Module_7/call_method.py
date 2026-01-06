779e2f0114df240e39a2329341399543389991ab
dmitr <dmitriyshirin86@gmail.com>
2026-01-06 20:46:54 +0100

    Use method __call__

---
+++ Module_7/call_method.py
@@ -0,0 +1,48 @@
+class Multiplier:
+    def __init__(self, factor):
+        self.factor = factor
+    
+    def __call__(self, other):
+        return self.factor * other
+
+# Створення екземпляра функтора
+double = Multiplier(2)
+triple = Multiplier(3)
+
+# Виклик функтора
+print(double(5))
+print(triple(3))
+
+# ---------------------------------------------------------------------------------------
+
+class Counter:
+    def __init__(self):
+        self.count = 0
+    
+    def __call__(self, *args, **kwargs):
+        self.count += 1
+
+counter = Counter()
+counter()
+counter()
+print(f"Call {counter.count} times.")
+
+# ---------------------------------------------------------------------------------------
+
+class SmartCalculator:
+    def __init__(self, operation='add'):
+        self.operation = operation
+    
+    def __call__(self, a, b):
+        if self.operation == 'add':
+            return a + b
+        elif self.operation == 'subtract':
+            return a - b
+        else:
+            return ValueError('Unknown operation')
+
+add = SmartCalculator('add')
+print(add(5, 3))
+
+subtract = SmartCalculator('subtract')
+print(subtract(10, 7))
