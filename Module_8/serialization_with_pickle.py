2c77e5b679b6e888f1904312fdee3ecb602037ba
dmitr <dmitriyshirin86@gmail.com>
2026-01-11 19:52:55 +0100

    Serialization with pickle

---
+++ Module_8/serialization_with_pickle.py
@@ -0,0 +1,66 @@
+import pickle
+
+# ----------------------------------------------------------------------------------------------------
+
+# Об'єкт для серіалізацї
+my_data = {'key': 'value', 'num': 42}
+
+# Серіалізація об'єкта в байтовий рядок
+serialized_data = pickle.dumps(my_data)
+# Виведе байтовий рядок
+print(serialized_data)
+
+# Десеріалізація об'єкта з байтового рядка
+deserialized_data = pickle.loads(serialized_data)
+# Виведе вихідний об'єкт Python
+print(deserialized_data)
+
+# ----------------------------------------------------------------------------------------------------
+
+# Об'єкт для серіалізації
+my_data = {'key': 'value', 'num': 100}
+
+# Серіалізація об'єкта в файл
+with open('data.pickle', 'wb') as file:
+    pickle.dump(my_data, file)
+
+# ----------------------------------------------------------------------------------------------------
+
+# Десеріалізація об'єкта з файлу
+with open('data.pickle', 'rb') as file:
+    deserialized_data = pickle.load(file)
+
+# Виведе вихідний об'єкт Python
+print(deserialized_data)
+
+# ----------------------------------------------------------------------------------------------------
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+bob = Human('Bob')
+with open('instance.pickle', 'wb') as file:
+    pickle.dump(bob, file)
+
+# ----------------------------------------------------------------------------------------------------
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+    
+with open('instance.pickle', 'rb') as file:
+    loaded_instance = pickle.load(file)
+
+print(loaded_instance.name)
+
+# ----------------------------------------------------------------------------------------------------
+
+#Збереження налаштувань
+settings = {'theme': 'dark', 'language': 'ukrainian'}
+with open('setting.pickle', 'wb') as file:
+    pickle.dump(settings, file)
+
+# Завантаження налаштувань
+with open('setting.pickle', 'rb') as file:
+    loaded_setting = pickle.load(file)
